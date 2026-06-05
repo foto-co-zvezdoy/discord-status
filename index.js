288825335018880402215fd26101dd3164a3de07
foto-co-zvezdoy <k56157602@gmail.com>
2026-06-05 14:54:43 +0300

    index.js

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ const TOKEN = process.env.DISCORD_TOKEN;
 const APP_ID = process.env.APPLICATION_ID;
 
 client.on('ready', async () => {
-  console.log(`СТАТУС УСПЕШНО ЗАПУЩЕН для аккаунта: ${client.user.tag}`);
+  console.log(`СТАТУС УСПЕШНО ОБНОВЛЕН: ${client.user.tag}`);
   
-  const startTime = Date.now() - (15000 * 60 * 60 * 1000);
+ 
+  const startTime = 0;
 
   client.user.setPresence({
     activities: [{
       name: "проводник", 
       type: "PLAYING",   
       applicationId: APP_ID,
-      details: "Пытаюсь найти твои мозги...", 
-      state: "Элемент не найден (Ошибка 404)",
+      details: "Пытаюсь найти твои мозги", 
+      state: "Элемент не найден",
       timestamps: { start: startTime }
     }],
     status: 'online'
@@ -32,7 +33,7 @@ client.on('ready', async () => {
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end("RPC Проводник работает 24/7!");
+  res.end("RPC Проводник работает на максимум!");
 }).listen(process.env.PORT || 3000);
 
 client.login(TOKEN).catch(err => {
