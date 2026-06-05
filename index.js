f71ae0ffcb03c1f51e0ce276c75db1954fa5e2a1
foto-co-zvezdoy <k56157602@gmail.com>
2026-06-05 14:21:18 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+try {
+  require.resolve('discord.js-selfbot-v13');
+} catch (e) {
+  require('child_process').execSync('npm install discord.js-selfbot-v13');
+}
+
 const { Client } = require('discord.js-selfbot-v13');
 const client = new Client({ checkUpdate: false });
 
@@ -5,8 +11,7 @@ const TOKEN = process.env.DISCORD_TOKEN;
 const APP_ID = process.env.APPLICATION_ID;
 
 client.on('ready', async () => {
-  console.log(`Статус успешно запущен для: ${client.user.tag}`);
-  
+  console.log(`СТАТУС УСПЕШНО ЗАПУЩЕН для аккаунта: ${client.user.tag}`);
   
   const startTime = Date.now() - (170000 * 60 * 60 * 1000);
 
@@ -26,7 +31,9 @@ client.on('ready', async () => {
 const http = require('http');
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  res.end("Статус работает круглосуточно!");
+  res.end("RPC Проводник работает 24/7!");
 }).listen(process.env.PORT || 3000);
 
-client.login(TOKEN);
+client.login(TOKEN).catch(err => {
+  console.error("ОШИБКА АВТОРИЗАЦИИ!");
+});
